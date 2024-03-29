b0aa8c953e85c2ee7c2d12628bfaeddb687b7ca1
sunny <sunnyy@gmail.com>
2024-03-29 15:59:49 +1000

    init

---
+++ gatherly/src/Gatherly.Persistence/Repositories/MemberRepository.cs
@@ -0,0 +1,17 @@
+﻿using Gatherly.Domain.Entities;
+using Gatherly.Domain.Repositories;
+
+namespace Gatherly.Persistence.Repositories;
+
+internal sealed class MemberRepository : IMemberRepository
+{
+    public Task<Member?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(Member member)
+    {
+        throw new NotImplementedException();
+    }
+}
